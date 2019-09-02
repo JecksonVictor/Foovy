dcc11cbb897c5723b95740ef02c2ed83b6b8de13
Gabriel <gabrielqap@hotmail.com>
2019-09-02 16:28:06 -0300

    teste: Inserção de funções
    Agora a função cria uma arquivo .groovy e insere nele as strings
    criadas. Além de verificar se possui os ":" no fim da frase,
    ignorando-os no .groovy.

--- Foovy.py
+++ Foovy.py
@@ -6,7 +6,16 @@ library = ["E", "Quando", "Dado", "Então", "Entao"]
 #Abrir arquivos
 input_file_name = sys.argv[1]
 input_file = open(input_file_name, "r")
-output_file = open(input_file_name + "_output", "w")
+output_file = open(input_file_name + ".groovy", "w")
+
+#adiciona as bibliotecas pt
+output_file.write("import cucumber.api.java.pt.E\n")
+output_file.write("import cucumber.api.java.pt.Dado\n")
+output_file.write("import cucumber.api.java.pt.Quando\n")
+output_file.write("import cucumber.api.java.pt.Entao\n")
+output_file.write("import internal.GlobalVariable\n\n")
+
+
 
 #Def da função de remover caracteres acentuados
 def clear(dirty_string):
@@ -20,11 +29,18 @@ def clear(dirty_string):
     dirty_string = dirty_string.replace('ú', 'u')
     return dirty_string
 
+#verifica sem a linha contem ":" no final
+def verifyLine(line):
+	if line:
+		if ":" in line[-1]:
+			print("ACHEI")
+			line = line[:-1]
+	return line
+
 #para cada linha do arquivo de entrada:
 for line in input_file:
     #remove identação e permite caracteres especiais
     line = line.strip().encode('latin-1').decode('utf-8')
-
     #verifica a primeira palavra da linha
     first_word = line.split(' ', 1)[0]
     
@@ -37,18 +53,20 @@ for line in input_file:
         if first_word in library:            
             #remove a primeira palavra da linha
             new_line = line[len(first_word)+1:]
-
+            new_line = verifyLine(new_line)
             #escreve a tag de identificação
             print ('@' + clear(first_word) + '("' + new_line + '")', end = '\n')
-
+            output_file.write("@" + clear(first_word) + '("' + new_line + '")' + '\n')
             #escreve a assinatura da função e a abertura das chaves
             print ("def ", end = '')
+            output_file.write("def ")
             word_list = new_line.split(' ')
             func_name = ''
             for s in word_list:
                 func_name += clear(s) + '_'
             func_name = func_name[:len(func_name)-1]
             print (func_name + '()' + '{' + '\n' + '\n' + '}' + '\n')
+            output_file.write(func_name + '()' + '{' + '\n' + '\n' + '}' + '\n')
         #caso contrário, nada a fazer
         else:
             pass
---
+++ input.groovy
@@ -0,0 +1,66 @@
+import cucumber.api.java.pt.E
+import cucumber.api.java.pt.Dado
+import cucumber.api.java.pt.Quando
+import cucumber.api.java.pt.Entao
+import internal.GlobalVariable
+
+@Entao("verifico que o campo de numero de processo esta presente")
+def verifico_que_o_campo_de_numero_de_processo_esta_presente(){
+
+}
+@E("verifico que o campo de ano de processo esta presente")
+def verifico_que_o_campo_de_ano_de_processo_esta_presente(){
+
+}
+@E("verifico que o campo de órgão esta presente")
+def verifico_que_o_campo_de_orgao_esta_presente(){
+
+}
+@E("verifico que o campo de relator esta presente")
+def verifico_que_o_campo_de_relator_esta_presente(){
+
+}
+@E("verifico que o campo de interessado esta presente")
+def verifico_que_o_campo_de_interessado_esta_presente(){
+
+}
+@E("verifico que o campo de assunto esta presente")
+def verifico_que_o_campo_de_assunto_esta_presente(){
+
+}
+@E("verifico que o botao de consulta esta presente")
+def verifico_que_o_botao_de_consulta_esta_presente(){
+
+}
+@Quando("realizo uma consulta que retorna resultados")
+def realizo_uma_consulta_que_retorna_resultados(){
+
+}
+@Entao("verifico que o link de visualizar esta presente")
+def verifico_que_o_link_de_visualizar_esta_presente(){
+
+}
+@Quando("não preencho nenhum campo")
+def nao_preencho_nenhum_campo(){
+
+}
+@E("clico no botão de consultar")
+def clico_no_botao_de_consultar(){
+
+}
+@Entao("a mensagem de campos mínimos para consulta é exibida")
+def a_mensagem_de_campos_minimos_para_consulta_e_exibida(){
+
+}
+@Quando("realizo uma consulta pública que retorna resultados")
+def realizo_uma_consulta_publica_que_retorna_resultados(){
+
+}
+@E("clico no link de visualizar processo da consulta pública")
+def clico_no_link_de_visualizar_processo_da_consulta_publica(){
+
+}
+@Entao("a página de detalhes do processo é exibida")
+def a_pagina_de_detalhes_do_processo_e_exibida(){
+
+}
