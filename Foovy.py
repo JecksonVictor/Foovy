fa1940d6cf5b773eb46b55a74a4325d62c3540b8
Jeckson Victor <jeckson@ufrn.edu.br>
2019-09-18 10:17:39 -0300

    Leitura de pasta inteira
    Passar uma pasta como argumento irá criar uma pasta de step definitions com os passos de todos os arquivos nas pastas e subpastas da pasta passada como argumento.
    É necessário que a pasta informada possua uma pasta interna chamada features (como no katalon)
    É possível passar apenas uma subpasta da pasta features
    É possível passar apenas um arquivo. E dessa forma ele será imprimido no terminal.

--- Foovy.py
+++ Foovy.py
@@ -1,60 +1,132 @@
+# #Abrir arquivos
+# input_file_name = sys.argv[1]
+# input_file = open(input_file_name, "r")
+# output_file = open(input_file_name[:-8] + ".groovy", "w")
+
+# placeHeader(output_file)
+# placeClassName(output_file, input_file_name)
+
+# #para cada linha do arquivo de entrada:
+# for line in input_file:
+#         #remove identação e permite caracteres especiais
+#     line = line.strip()
+
+#     #verifica a primeira palavra da linha
+#     first_word = line.split(' ', 1)[0]
+    
+#     #se a primeira palavra for um cenário, insira como comentário
+#     if first_word == "Cenário:" or first_word == "Esquema":
+#         output_file.write('\t' + '/*' + line + '*/' + '\n')
+#     #senão, verifique se é uma palavra reservada
+#     else:
+#         #se primeira palavra é palavra reservada
+#         if first_word in getReservedWords():            
+#             #remove a primeira palavra da linha
+#             new_line = line[len(first_word)+1:]
+
+#             #remove ':' no fim da linha
+#             new_line = verifyLine(new_line)
+
+#             #escreve a tag de identificação
+#             output_file.write('\t' + "@" + clear(first_word) + '("' + new_line + '")' + '\n')
+
+#             #escreve a assinatura da função e a abertura das chaves
+#             output_file.write('\t' + "def ")
+#             word_list = new_line.split(' ')
+#             func_name = ''
+#             for s in word_list:
+#                 func_name += clear(s) + '_'
+#             func_name = func_name[:len(func_name)-1]
+#             output_file.write(func_name + '()' + '{' + '\n' + '\t' + '\t' + '\n' + '\t' + '}' + '\n' + '\n')
+
+#         #caso contrário, nada a fazer
+#         else:
+#             pass
+
+# #insere o final brace
+# output_file.write('}')
+
+# #fecha os arquivos abertos
+# input_file.close()
+# output_file.close()
+
+
+import os
 import sys
 from Functions import *
 
-#Abrir arquivos
-input_file_name = sys.argv[1]
-input_file = open(input_file_name, "r")
-output_file = open(input_file_name[:-8] + ".groovy", "w")
+# Local com todos os arquivos feature desejados "caminho_do_projeto\Include\features"
+input_value = str(sys.argv[1])
 
-placeHeader(output_file)
-placeClassName(output_file, input_file_name)
+input_files = []
 
-#para cada linha do arquivo de entrada:
-for line in input_file:
-        #remove identação e permite caracteres especiais
-    line = line.strip()
+# coleta todos os arquivos feature
+if os.path.isdir(input_value):
+    # r = root, d = directories, f = files
+    for r, d, f in os.walk(input_value):
+        for file in f:
+            if '.feature' in file:
+                input_files.append(os.path.join(r, file))
+else:
+    # os.path.isfile(input_value) == true
+    input_files.append(input_value)
 
-    #verifica a primeira palavra da linha
-    first_word = line.split(' ', 1)[0]
-    
-    #se a primeira palavra for um cenário, insira como comentário
-    if first_word == "Cenário:" or first_word == "Esquema":
-        output_file.write('\t' + '/*' + line + '*/' + '\n')
-    #senão, verifique se é uma palavra reservada
-    else:
-        #se primeira palavra é palavra reservada
-        if first_word in getReservedWords():            
-            #remove a primeira palavra da linha
-            new_line = line[len(first_word)+1:]
+for input_file_name in input_files:
+    output_file_name = input_file_name[0:input_file_name.find('features')] + "scripts\groovy\\" + input_file_name[input_file_name.find('features')+9:-8] + ".groovy"
+    os.makedirs(os.path.dirname(output_file_name), exist_ok=True)
+    try:
+        with open(output_file_name, 'x', encoding="utf8") as output_file:
+            
+            placeHeader(output_file)
+            placeClassName(output_file, input_file_name[input_file_name.rfind(' '):])
 
-            #remove ':' no fim da linha
-            new_line = verifyLine(new_line)
+            input_file = open(input_file_name, "r", encoding="utf8")
+            #para cada linha do arquivo de entrada:
+            for line in input_file:
+                #remove identação
+                line = line.strip()
 
-            #escreve a tag de identificação
-            output_file.write('\t' + "@" + clear(first_word) + '("' + new_line + '")' + '\n')
+                #verifica a primeira palavra da linha
+                first_word = line.split(' ', 1)[0]
+                
+                #se a primeira palavra for um cenário, insira como comentário
+                if first_word == "Cenário:" or first_word == "Esquema":
+                    output_file.write('\t' + '/*' + line + '*/' + '\n')
+                #senão, verifique se é uma palavra reservada
+                else:
+                    #se primeira palavra é palavra reservada
+                    if first_word in getReservedWords():            
+                        #remove a primeira palavra da linha
+                        new_line = line[len(first_word)+1:]
 
-            #escreve a assinatura da função e a abertura das chaves
-            output_file.write('\t' + "def ")
-            word_list = new_line.split(' ')
-            func_name = ''
-            for s in word_list:
-                func_name += clear(s) + '_'
-            func_name = func_name[:len(func_name)-1]
-            output_file.write(func_name + '()' + '{' + '\n' + '\t' + '\t' + '\n' + '\t' + '}' + '\n' + '\n')
+                        #remove ':' no fim da linha
+                        new_line = verifyLine(new_line)
 
-        #caso contrário, nada a fazer
-        else:
-            pass
+                        #escreve a tag de identificação
+                        output_file.write('\t' + "@" + clear(first_word) + '("' + new_line + '")' + '\n')
 
-#insere o final brace
-output_file.write('}')
+                        #escreve a assinatura da função e a abertura das chaves
+                        output_file.write('\t' + "def ")
+                        word_list = new_line.split(' ')
+                        func_name = ''
+                        for s in word_list:
+                            func_name += clear(s) + '_'
+                        func_name = func_name[:len(func_name)-1]
+                        output_file.write(func_name + '()' + '{' + '\n' + '\t' + '\t' + '\n' + '\t' + '}' + '\n' + '\n')
 
-#fecha os arquivos abertos
-input_file.close()
-output_file.close()
+                    #caso contrário, nada a fazer
+                    else:
+                        pass
+            #insere o final brace
+            output_file.write('}')
 
-#print outfile
-with open(input_file_name[:-8] + ".groovy", "r") as outfile:
-    for l in outfile:
-        l = l.encode('latin-1').decode('utf-8')
-        print(l, end = "")
+            output_file.close()
+
+    except FileExistsError:
+        print ("Arquivo já existente: " + output_file_name)
+
+if len(input_files) == 1:
+    with open(output_file_name, "r") as outfile:
+        for l in outfile:
+            l = l.encode('latin-1').decode('utf-8')
+            print(l, end = "")
